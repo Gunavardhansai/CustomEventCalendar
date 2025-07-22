e861c1f0ea5de4c00496fe0d9758c0338face064
GUNAVARDHANSAI PUTTA <96119286+Gunavardhansai@users.noreply.github.com>
2025-07-22 22:13:56 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,42 @@
 export const EVENT_COLORS = [
-  { value: 'blue', label: 'Blue', class: 'bg-blue-500' },
-  { value: 'green', label: 'Green', class: 'bg-green-500' },
-  { value: 'red', label: 'Red', class: 'bg-red-500' },
-  { value: 'purple', label: 'Purple', class: 'bg-purple-500' },
-  { value: 'yellow', label: 'Yellow', class: 'bg-yellow-500' },
-  { value: 'pink', label: 'Pink', class: 'bg-pink-500' },
+  {
+    value: 'blue',
+    label: 'Blue',
+    bg: 'bg-blue-100',
+    border: 'border-blue-500'
+  },
+  {
+    value: 'green',
+    label: 'Green',
+    bg: 'bg-green-100',
+    border: 'border-green-500'
+  },
+  {
+    value: 'red',
+    label: 'Red',
+    bg: 'bg-red-100',
+    border: 'border-red-500'
+  },
+  {
+    value: 'purple',
+    label: 'Purple',
+    bg: 'bg-purple-100',
+    border: 'border-purple-500'
+  },
+  {
+    value: 'yellow',
+    label: 'Yellow',
+    bg: 'bg-yellow-100',
+    border: 'border-yellow-500'
+  },
+  {
+    value: 'pink',
+    label: 'Pink',
+    bg: 'bg-pink-100',
+    border: 'border-pink-500'
+  }
 ];
+
 
 export const RECURRENCE_OPTIONS = [
   { value: 'none', label: 'No Repeat' },
